766a2bc79e187689887193291ad296b4c4e5ca85
YA7MO <ya7yamo1@gmail.com>
2019-01-22 16:58:12 +0300

    back end

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 console.log('here we go');
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
